2e3c496687f2fa748588c4a7399ef48c1275d3eb
TemaTerbi <tematerbi@mail.ru>
2023-12-23 11:51:24 +0700

    feat: test

--- Sources/VacanciesParser/VacanciesParser.swift
+++ Sources/VacanciesParser/VacanciesParser.swift
@@ -1,7 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import Foundation
 
-public struct VacanciesParser {
+public class VacanciesParser {
     func testPrint() {
         NetworkManager.shared.getVacancies { str in
             print(str)
