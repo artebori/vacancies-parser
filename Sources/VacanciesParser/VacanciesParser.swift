9286b30517d89cdafb341d8149c64b655dd98a3c
TemaTerbi <tematerbi@mail.ru>
2023-12-23 11:53:41 +0700

    feat: create public methodt

--- Sources/VacanciesParser/VacanciesParser.swift
+++ Sources/VacanciesParser/VacanciesParser.swift
@@ -1,9 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
 import Foundation
 
 public class VacanciesParser {
-    func testPrint() {
+   public func testPrint() {
         NetworkManager.shared.getVacancies { str in
             print(str)
         }
